32f3e8494c949267ed5ee5dc102dc53841b870a8
AnastasiaZabor <106294576+AnastasiaZabor@users.noreply.github.com>
2025-11-18 22:52:54 +0700

    Знакомство с PageObject

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,19 +1,15 @@
 import pytest
 from playwright.sync_api import sync_playwright, expect, Page
+from pages.registration_page import RegistrationPage
+from pages.dashboard_page import DashboardPage
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration(chromium_page:Page):  
-        chromium_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+@pytest.mark.parametrize("email, username, password", [("user.name@gmail.com", "test", "password")])
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage, email: str, username: str, password: str):  
 
-        email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user@gmail.com')
+        registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+        registration_page.fill_registration_form(email=email, username=username, password=password)
+        registration_page.click_registration_button()
+        dashboard_page.check_dashboard_title()
 
-        username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
-
-        password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
-
-        registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
