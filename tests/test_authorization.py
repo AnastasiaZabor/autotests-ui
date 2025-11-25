ffaca6f12a911d64e31a404824f95b2a37ce8af6
AnastasiaZabor <106294576+AnastasiaZabor@users.noreply.github.com>
2025-11-25 22:21:22 +0700

    Знакомство с PageFactory

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,5 +1,5 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect, Page
+from playwright.sync_api import sync_playwright
 from pages.login_page import LoginPage
 
 @pytest.mark.regression
