ddcc701607912f7595ed64786fc2646ad51e84b7
AnastasiaZabor <106294576+AnastasiaZabor@users.noreply.github.com>
2025-11-26 12:21:52 +0700

    Практическое применение PageFactory в автотестах

--- elements/base_element.py
+++ elements/base_element.py
@@ -7,9 +7,9 @@ class BaseElement:
         self.name = name
         self.locator = locator
 
-    def get_locator(self, **kwargs) -> Locator:
+    def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         locator = self.locator.format(**kwargs)
-        return self.page.get_by_test_id(locator)
+        return self.page.get_by_test_id(locator).nth(nth)
 
     def click(self, **kwargs):
         locator = self.get_locator(**kwargs)
